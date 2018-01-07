7dcd6694db2ee477053dde531daf769ebf00d5e6
Edgar Rios <edgar.rios@hacom.com.pe>
2018-01-07 12:50:21 -0500

    Initial commit

---
+++ src/main/groovy/com/gs/bcocencosud/BcocencosudApplication.groovy
@@ -0,0 +1,12 @@
+package com.gs.bcocencosud
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BcocencosudApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BcocencosudApplication, args
+	}
+}
